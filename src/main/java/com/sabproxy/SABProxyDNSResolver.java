d2db44784f05613befb40858a2af47418883f96e
Pedro Nunes <pgh.nunes@gmail.com>
2017-04-18 23:01:41 +0100

    Rename package

---
+++ src/main/java/com/sabproxy/SABProxyDNSResolver.java
@@ -0,0 +1,31 @@
+package com.sabproxy;
+
+import org.littleshoot.proxy.HostResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+
+public class SABProxyDNSResolver implements HostResolver {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private AdServers adServers = null;
+
+    public SABProxyDNSResolver(AdServers adServers) {
+        this.adServers = adServers;
+    }
+
+    @Override
+    public InetSocketAddress resolve(String host, int port) throws UnknownHostException {
+        if (adServers.contains(host)) {
+            log.info("[" + adServers.getSessionBlockedAds() + "] Blocked Ad request from: " + host);
+            InetAddress serverInetAddr = InetAddress.getByName("127.0.0.1");
+            return new InetSocketAddress(serverInetAddr, 0);
+        }
+
+        InetAddress addr = InetAddress.getByName(host);
+        return new InetSocketAddress(addr, port);
+    }
+
+}
