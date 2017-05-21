d58649c21a91c83f070e7fa3605028fcd872ccfe
Pedro Nunes <pgh.nunes@gmail.com>
2017-05-21 21:50:13 +0100

    Custom response for (http) ad requests

--- src/main/java/com/sabproxy/SABPServer.java
+++ src/main/java/com/sabproxy/SABPServer.java
@@ -9,8 +9,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
--- src/main/java/com/sabproxy/SystemInfoUtil.java
+++ src/main/java/com/sabproxy/SystemInfoUtil.java
@@ -48,41 +48,41 @@ public class SystemInfoUtil {
         NetworkIF[] netIFS = hal.getNetworkIFs();
         String netInfo = "";
         for (NetworkIF net : netIFS) {
-            netInfo += "Interface: "+net.getName()+"\n";
-            netInfo += "MAC Address: "+net.getMacaddr()+"\n";
-            netInfo += "IPv4: "+Arrays.toString(net.getIPv4addr())+"\n";
-            if(net.getIPv6addr().length>0) {
+            netInfo += "Interface: " + net.getName() + "\n";
+            netInfo += "MAC Address: " + net.getMacaddr() + "\n";
+            netInfo += "IPv4: " + Arrays.toString(net.getIPv4addr()) + "\n";
+            if (net.getIPv6addr().length > 0) {
                 netInfo += "IPv6: " + Arrays.toString(net.getIPv6addr()) + "\n";
             }
-            netInfo += "Received: "+net.getPacketsRecv()+" packets / "+FormatUtil.formatBytes(net.getBytesRecv())+" ("+net.getInErrors()+" err)\n";
-            netInfo += "Transmitted: "+net.getPacketsSent()+" packets / "+FormatUtil.formatBytes(net.getBytesSent())+" ("+net.getOutErrors()+" err)\n";
+            netInfo += "Received: " + net.getPacketsRecv() + " packets / " + FormatUtil.formatBytes(net.getBytesRecv()) + " (" + net.getInErrors() + " err)\n";
+            netInfo += "Transmitted: " + net.getPacketsSent() + " packets / " + FormatUtil.formatBytes(net.getBytesSent()) + " (" + net.getOutErrors() + " err)\n";
             netInfo += "\n";
         }
         return netInfo;
     }
 
-    public String getNetworkParameters(){
+    public String getNetworkParameters() {
         NetworkParams networkParams = os.getNetworkParams();
-        String netParams = "Host name: "+networkParams.getHostName()+"\n";
-        netParams += "Domain name: "+networkParams.getDomainName()+"\n";
-        netParams += "DNS servers: "+Arrays.toString(networkParams.getDnsServers())+"\n";
-        if(!networkParams.getIpv4DefaultGateway().equals("")) {
+        String netParams = "Host name: " + networkParams.getHostName() + "\n";
+        netParams += "Domain name: " + networkParams.getDomainName() + "\n";
+        netParams += "DNS servers: " + Arrays.toString(networkParams.getDnsServers()) + "\n";
+        if (!networkParams.getIpv4DefaultGateway().equals("")) {
             netParams += "IPv4 Gateway: " + networkParams.getIpv4DefaultGateway() + "\n";
         }
-        if(!networkParams.getIpv6DefaultGateway().equals("")) {
+        if (!networkParams.getIpv6DefaultGateway().equals("")) {
             netParams += "IPv6 Gateway: " + networkParams.getIpv6DefaultGateway() + "\n";
         }
 
         return netParams;
     }
 
-    public String getSensorsInfo(){
+    public String getSensorsInfo() {
         Sensors sensors = hal.getSensors();
-        String sensorsInfo = "CPU Temperature: "+sensors.getCpuTemperature()+"°C\n";
-        if(sensors.getFanSpeeds().length > 0) {
+        String sensorsInfo = "CPU Temperature: " + sensors.getCpuTemperature() + "°C\n";
+        if (sensors.getFanSpeeds().length > 0) {
             sensorsInfo += "Fan Speeds: " + Arrays.toString(sensors.getFanSpeeds()) + "\n";
         }
-        if(sensors.getCpuVoltage() > 0) {
+        if (sensors.getCpuVoltage() > 0) {
             sensorsInfo += "CPU Voltage: " + sensors.getCpuVoltage() + "\n";
         }
         return sensorsInfo;
